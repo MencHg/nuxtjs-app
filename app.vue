8bfb10dd7b84b48eebb6e71545d39982b9376842
web_menc <273381276@qq.com>
2020-03-18 20:32:18 +0800

    nuxt-project-initalize

---
+++ app.vue
@@ -0,0 +1,32 @@
+<template>
+  <section class="home">this is my component page</section>
+</template>
+<script>
+export default {
+  name: "home",
+  components: {
+
+  },
+  data: () => ({
+
+  }),
+  watch: {
+
+  },
+  filters: {
+
+  },
+  created() {
+
+  },
+  methods: {
+
+  },
+  mounted(){
+
+  },
+};
+</script>
+<style lang="less" scoped>
+  
+</style>
